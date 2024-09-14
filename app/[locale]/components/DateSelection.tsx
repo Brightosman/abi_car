99a4b91890e9576432eb2d3e757408a5699e4573
Brightosman <brichiile@outlook.com>
2024-09-14 12:38:27 +0200

    0.0.007

--- app/[locale]/components/DateSelection.tsx
+++ app/[locale]/components/DateSelection.tsx
@@ -10,7 +10,7 @@ import { FaCalendarAlt } from 'react-icons/fa';
 import { FaArrowRightLong } from 'react-icons/fa6';
 
 // react date
-import { DateRange } from 'react-date-range';
+import { DateRange, Range } from 'react-date-range';
 import { format, addDays } from 'date-fns';
 
 // react date range css
@@ -18,42 +18,44 @@ import 'react-date-range/dist/styles.css';
 import 'react-date-range/dist/theme/default.css';
 
 export default function DateSelection() {
-  const [date, setDate] = useState([
+  // Use the Range type from the react-date-range library
+  const [date, setDate] = useState<Range[]>([
     {
       startDate: new Date(),
-      endDate: null,
+      endDate: undefined,
       key: 'selection',
     },
   ]);
 
+  const handleSelect = (ranges: any) => {
+    const selection = ranges.selection;
+    setDate([selection]);
+  };
+
   return (
-    <Menu as='div' className='w-full h-full flex xl:flex-row'>
-      <div className='relative flex-1'>
-        {/* btn */}
-        <MenuButton className='dropdown-btn w-full h-full flex flex-col justify-center items-center xl:items-start xl:pl-8'>
-          <div className='flex flex-col xl:flex-row items-center xl:gap-x-2 gap-y-2 xl:gap-y-0'>
-            <FaCalendarAlt className='text-accent' />
-            <div className='text-[15px] uppercase font-bold'>Select Date</div>
+    <Menu as="div" className="w-full h-full flex xl:flex-row">
+      <div className="relative flex-1">
+        {/* button */}
+        <MenuButton className="dropdown-btn w-full h-full flex flex-col justify-center items-center xl:items-start xl:pl-8">
+          <div className="flex flex-col xl:flex-row items-center xl:gap-x-2 gap-y-2 xl:gap-y-0">
+            <FaCalendarAlt className="text-accent" />
+            <div className="text-[15px] uppercase font-bold">Select Date</div>
           </div>
-          <div className='flex items-center gap-x-3 xl:ml-6'>
-            <div className='text-[13px] font-medium text-secondary'>
-              {format(date[0].startDate, 'dd/MM/yyy')}
+          <div className="flex items-center gap-x-3 xl:ml-6">
+            <div className="text-[13px] font-medium text-secondary">
+              {date[0].startDate ? format(date[0].startDate, 'dd/MM/yyyy') : 'Start Date'}
             </div>
-            <FaArrowRightLong className='text-accent text-[12px]' />
-            <div className='text-[13px] font-medium text-secondary'>
-              {date[0].endDate ? (
-                <div>{format(date[0].endDate, 'dd/MM/yyyy')}</div>
-              ) : (
-                <div>{format(date[0].startDate, 'dd/MM/yyyy')}</div>
-              )}
+            <FaArrowRightLong className="text-accent text-[12px]" />
+            <div className="text-[13px] font-medium text-secondary">
+              {date[0].endDate ? format(date[0].endDate as Date, 'dd/MM/yyyy') : 'End Date'}
             </div>
           </div>
         </MenuButton>
 
-        {/* menu */}
-        <MenuItems className='drodown-menu shadow-lg absolute -top-96 xl:top-[90px] left-1/2 xl:left-0 z-50 transform -translate-x-1/2 xl:-translate-x-0 rounded-[10px] overflow-hidden'>
+        {/* date picker menu */}
+        <MenuItems className="dropdown-menu shadow-lg absolute -top-96 xl:top-[90px] left-1/2 xl:left-0 z-50 transform -translate-x-1/2 xl:-translate-x-0 rounded-[10px] overflow-hidden">
           <DateRange
-            onChange={(item) => setDate([item.selection])}
+            onChange={handleSelect}
             editableDateInputs={true}
             moveRangeOnFirstSelection={false}
             ranges={date}
