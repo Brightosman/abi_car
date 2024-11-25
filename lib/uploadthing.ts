abe7ba80b888b4d2b93439580b4459b498692d21
Brightosman <brichiile@outlook.com>
2024-11-25 23:48:27 +0100

    0.0.012

--- lib/uploadthing.ts
+++
@@ -1,7 +0,0 @@
-import { createUploadthing } from "uploadthing/next";
-
-const uploadthing = createUploadthing();
-
-export const carImageUpload = uploadthing({
-  file: { maxSize: "4MB", accepted: ["image/*"] },
-});
