af271808942a37164fb9bea1a774b2694949cb6d
Brightosman <brichiile@outlook.com>
2024-12-04 03:20:00 +0100

    0.0.021

--- app/[locale]/(actions)/make.ts
+++ app/[locale]/(actions)/make.ts
@@ -39,7 +39,7 @@ export async function AddMake(prevState: any, formData: FormData){
         return state;
     }
 
-    await prisma.Make.create({
+    await prisma.make.create({
         data: {
             title: validateFields.data.title,
             logoUrl: validateFields.data.logoUrl,
--- app/[locale]/lib/db.ts
+++ app/[locale]/lib/db.ts
@@ -1,21 +1,37 @@
-import { PrismaClient } from '@prisma/client';
+// import { PrismaClient } from '@prisma/client';
 
-let prisma: PrismaClient;
+// let prisma: PrismaClient;
 
-declare global {
-  // Allow global `var`s in TypeScript
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
+// declare global {
+//   // Allow global `var`s in TypeScript
+//   // eslint-disable-next-line no-var
+//   var prisma: PrismaClient | undefined;
+// }
+
+// if (process.env.NODE_ENV === 'production') {
+//   prisma = new PrismaClient();
+// } else {
+//   if (!global.prisma) {
+//     global.prisma = new PrismaClient();
+//   }
+//   prisma = global.prisma;
+// }
+
+// export const db = globalThis.prisma || new PrismaClient();
+// export { prisma };
+
+import { PrismaClient } from '@prisma/client'
+
+const prismaClientSingleton = () => {
+  return new PrismaClient()
 }
 
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
 
-export const db = globalThis.prisma || new PrismaClient();
-export { prisma };
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+
+export default prisma
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
